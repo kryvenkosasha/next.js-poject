d716d9bf9f3ef7cd7a304285df82989e1bb938dc
Olena Kryvenko <sashakryvenko2006@gmail.com>
2024-08-04 11:44:56 +0200

    add status labels

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
-
+import StatusLabel, {Status} from "@/components/StatusLabel/StatusLabel";
 
 export default function Home() {
   return (
     <main>
-      <h1>Home page</h1>
+      <h1 className="text-xl">Home page</h1>
+      <StatusLabel status={Status.Active}>Active</StatusLabel>
+      <StatusLabel status={Status.NotActive}>Not Active</StatusLabel>
+      <StatusLabel status={Status.Pending}>Pending</StatusLabel>
+      <StatusLabel status={Status.Suspended}>Suspended</StatusLabel>
     </main>
   );
 }
